d4f3649a0074ed18f9e067c159c6b3c848ec3e15
Simon <simon_wzm@163.com>
2022-12-01 18:34:07 +0800

    right content container created

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -8,7 +8,7 @@
         size="large"
          />
       </div>
-      <div class="more-searchbox mt-6 overflow-hidden">
+      <div class="more-searchbox mt-6 h-3/4 ">
           <a-input-group size="large" >
           <a-row :gutter="20">
             <a-col :span="4">
@@ -16,9 +16,6 @@
             </a-col>
             <a-col :span="4">
               <a-input addon-before="title" ></a-input>
-            </a-col>
-            <a-col :span="4">
-              <a-input addon-before="author" ></a-input>
             </a-col>
             <a-col :span="4">
               <a-date-picker placeholder="select date" />
@@ -30,8 +27,8 @@
 
          </div>
     </div>
-    <div class="left-container h-3/4 w-2/3 float-left ml-12">
-        <div class="answer-container h-3/4   ">
+    <div class="left-container h-3/4 float-left ml-12" style='width:60%;'>
+        <div class="answer-container overflow-y-scroll" style="height:580px;" v-if="isListAnswer">
             <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
                 <!-- <div slot="footer"><b>ant design vue</b> footer part</div> -->
                 <a-list-item slot="renderItem" key="item.title" slot-scope="item, index">
@@ -57,12 +54,57 @@
             <!-- <a-pagination :total="50" style="margin-top: 32px; text-align: center" /> -->
             </a-list>
         </div>
+        <div class="pic-container overflow-y-scroll bg-black" style="height:580px;" v-if="!isListAnswer">
+        </div>
     </div>
     
-    <HR style="FILTER: progid:DXImageTransform.Microsoft.Shadow(color:#987cb9,direction:145,strength:15)" width="80%" color=#987cb9 SIZE=1></HR>
+    <HR style="z-index: -1;FILTER: progid:DXImageTransform.Microsoft.Shadow(color:#987cb9,direction:145,strength:15)" width="80%" color=#987cb9 SIZE=1></HR>
 
-    <div class="right-container w-1/4 float-right h-3/4 bg-black mt-6">
+    <div class="right-container w-1/4 mx-12 float-right h-3/4 " style="width:30%;">
+      <div style="margin-top:-44px; z-index:12;">
+          <a-tabs default-active-key="1" @change="callback" style="z-index:12">
+            <a-tab-pane key="1" tab="Tab 1" style="z-index:12">
+              <a-card hoverable style="width: 300px">
+                  <img
+                    slot="cover"
+                    alt="example"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                  />
+                  <template slot="actions" class="ant-card-actions">
+                    <a-icon key="setting" type="setting" />
+                    <a-icon key="edit" type="edit" />
+                    <a-icon key="ellipsis" type="ellipsis" />
+                  </template>
+                  <a-card-meta title="Card title" description="This is the description">
+                    <a-avatar
+                      slot="avatar"
+                      src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                    />
+                  </a-card-meta>
+                </a-card>
+            </a-tab-pane>
+            <a-tab-pane key="2" tab="Tab 2" force-render style="z-index:12">
+              <template>
+                <a-card hoverable style="width: 240px">
+                  <img
+                    slot="cover"
+                    alt="example"
+                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                  />
+                  <a-card-meta title="Europe Street beat">
+                    <template slot="description">
+                      www.instagram.com
+                    </template>
+                  </a-card-meta>
+                </a-card>
+              </template>
 
+            </a-tab-pane>
+            <a-tab-pane key="3" tab="Tab 3" style="z-index:12">
+              Content of Tab Pane 3
+            </a-tab-pane>
+          </a-tabs>
+        </div>
     </div>
     </div>
 </template>
@@ -89,6 +131,7 @@ export default {
   data() {
     return {
       listData,
+      isListAnswer: false,
       pagination: {
         onChange: page => {
           console.log(page);
@@ -102,6 +145,11 @@ export default {
       ],
     };
   },
+  methods: {
+    callback(key) {
+      console.log(key);
+    },
+  },
 };
 </script>
 <style scoped>
@@ -113,6 +161,15 @@ export default {
     background-image: "@/assets/images/bg5.jpg";
     z-index:-2;
 } */
+*::-webkit-scrollbar {
+    display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+*{
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
 
 a-input-search {
 
